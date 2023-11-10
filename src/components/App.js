66bf2ee42cfc34ae059530fd3ead8dfdcbedb719
danil emelin <danil228aeedj@mail.ru>
2023-11-10 22:09:58 +0300

    use new web3 methods

--- src/components/App.js
+++ src/components/App.js
@@ -9,49 +9,23 @@ import DecentralBank from '../truffle_abis/DecentralBank.json'
 
 class App extends Component{
   async componentWillMount(){
-    //always take new addresses from metaMASK.
+    //always take new addresses from metaMASK,recompile sol contracts, add them to truffle abis.
     await this.loadWeb3()
     await this.loadBlockchainData();
   }
   async loadBlockchainData(){
 
-    const web3=window.web3
+    const web3=await window.web3
     const account= await web3.eth.getAccounts()
     this.setState({account:account[0]})
     const networkId=await web3.eth.net.getId();
     console.log(networkId," NETWORK ID")
-    console.log(account)
+    console.log(account," ACCOUNTS")
     const tetherData=Tether.networks[networkId];
     const rwdDAta=RWD.networks[networkId];
     const decentralBank=DecentralBank.networks[networkId];
 
-    
-    if (decentralBank){
-      //take tether contract.
-      const bank=new web3.eth.Contract(DecentralBank.abi,decentralBank.address);
-      this.setState({bank});
-      //take balance of bank 
-      let stakingBalance=await bank.methods.stakingBalance(this.state.account).call();
-      this.setState({stakingBalance:stakingBalance})
-    }else{
-      window.alert('Error!!decentralBank contract not deployed')
-    }
-    
-
-
-    if (rwdDAta){
-      //take tether contract.
-      const rwd=new web3.eth.Contract(RWD.abi,rwdDAta.address);
-      this.setState({rwd});
-      //take balance of bank 
-      let rwdBalance=await rwd.methods.balance(this.state.account).call();
-      this.setState({rwdBalance:rwdBalance})
-      console.log(rwdBalance," BALANCE RWD BANK" )
-    }else{
-      window.alert('RWD contract not deployed')
-    }
-    
-    
+      
     if (tetherData){
       //take tether contract.
       const tether=new web3.eth.Contract(Tether.abi,tetherData.address);
@@ -63,18 +37,52 @@ class App extends Component{
     }else{
       window.alert('Tether contract not deployed')
     }
+     
+    if (decentralBank){
+      //take tether contract.
+      const bank=new web3.eth.Contract(DecentralBank.abi,decentralBank.address);
+      this.setState({bank});
+      //take balance of bank 
+      let stakingBalance=await bank.methods.stakingBalance(this.state.account).call();
+      this.setState({stakingBalance:stakingBalance})
+    }else{
+      window.alert('Error!!decentralBank contract not deployed')
+    }
+    if (rwdDAta){
+      //take tether contract.
+      const rwd=new web3.eth.Contract(RWD.abi,rwdDAta.address);
+      this.setState({rwd});
+      //take balance of bank 
+      let rwdBalance=await rwd.methods.balance(this.state.account).call();
+      this.setState({rwdBalance:rwdBalance})
+      console.log(rwdBalance," BALANCE RWD BANK" )
+    }else{
+      window.alert('RWD contract not deployed')
+    }
+
     this.setState({loading:false})
   }
 
     async loadWeb3(){
-    if (window.ethereuem){
-        window.web3=new Web3(window.ethereuem);
-        await window.ethereuem.enable()
-    }else{
-        if (window.web3){
-            window.web3=new Web3(window.web3.currentProvider);
-        }else{window.alert('No ethreuem window detected!Check MetaMask')}
-    }
+      if (window.ethereum) {
+        App.web3Provider = window.ethereum;
+        try {
+          // Request account access
+          await window.ethereum.enable();
+        } catch (error) {
+          // User denied account access...
+          console.error("User denied account access")
+        }
+      }
+      // Legacy dapp browsers...
+      else if (window.web3) {
+        App.web3Provider = window.web3.currentProvider;
+      }
+      // If no injected web3 instance is detected, fall back to Ganache
+      else {
+        App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      }
+      window.web3 = new Web3(App.web3Provider);
   }
     constructor(props){
         super(props)
