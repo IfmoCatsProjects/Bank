b1ab948a018b9c6540c7ab0a12a28acb2d23aa28
danil emelin <danil228aeedj@mail.ru>
2023-11-10 12:47:41 +0300

    Add Mocha and Chai Tests

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,10 +5,14 @@ const DecentralBank=artifacts.require ('DecentralBank');
 module.exports=async function (deployer,network,accounts){
     await deployer.deploy(Tether);
     const tether=await Tether.deployed();
+    
     await deployer.deploy(RWD);
     const rwd=await RWD.deployed();
-    await deployer.deploy(DecentralBank);
+    
+    await deployer.deploy(DecentralBank,rwd.address,tether.address);
     const decentralBank=await DecentralBank.deployed();
 
-rwd.transfer(DecentralBank.address,'1000000000000000000000000');
+await rwd.transfer(decentralBank.address,'1000000000000000000000000');
+await tether.transfer(accounts[1],'1000000000000000000');
+
 }
